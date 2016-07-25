98e984d8da0a93e799056ab70097acb38f013f61
Tom Esterez <testerez@gmail.com>
2016-07-24 22:00:33 -0400

    Simplify Box.ts + change box size

--- src/mines/data/Box.ts
+++ src/mines/data/Box.ts
@@ -4,18 +4,14 @@ import {observable} from 'mobx';
 export default class{
   @observable position: number;
   @observable hasMine: boolean;
-  @observable isRevealed: boolean = false;
-  @observable isFlagged: boolean = false;
+  @observable isRevealed = false;
+  @observable isFlagged = false;
 
   constructor(
     position: number,
     hasMine: boolean,
-    isRevealed: boolean = false,
-    isFlagged: boolean = false
   ){
     this.position = position;
     this.hasMine = hasMine;
-    this.isRevealed = isRevealed;
-    this.isFlagged = isFlagged;
   }
 }
