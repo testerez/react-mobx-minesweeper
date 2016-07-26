04d2a3e1ab41494e8be319f38dac25336724e986
Tom Esterez <testerez@gmail.com>
2016-07-26 09:18:16 -0400

    Fix enzyme errors and remove noise from karma log

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ Todo:
 - polyfills
 - isdev
 - fix sourcemap in karma tests
-- fix ezyme errors
 - test components
 - drop unused node modules
+- routing
--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,20 @@ module.exports = function (config) {
     },
     webpack: {
       module: webpackConfig.module,
-      resolve: webpackConfig.resolve
+      resolve: webpackConfig.resolve,
+      externals: {
+        // Those are conditional imports from enzime that
+        // are used only for older React versions and generate
+        // errors in build.
+        // See: https://github.com/airbnb/enzyme/issues/47#issuecomment-162256140
+        "react/lib/ExecutionEnvironment": "window",
+        "react/lib/ReactContext": "window",
+      },
+    },
+    webpackMiddleware: {
+      stats: {
+        chunks: false,
+      },
     },
     reporters: ['progress'],
     port: 9876,
